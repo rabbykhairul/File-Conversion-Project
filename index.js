1f30e0514e19d6b0393abf346245007dfee8f929
rabbykhairul <rabbykhairul@gmail.com>
2020-09-26 14:19:20 +0600

    add multer middleware for file upload

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const multer = require('multer');
 const app = express();
+const upload = multer({dest: 'uploads/'});
 const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
 
 app.listen(port, () => {
     console.log(`Started listening at port ${port}...`);

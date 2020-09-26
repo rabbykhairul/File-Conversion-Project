15e68667e203281b851c73fde5a0e96bfb0d301d
rabbykhairul <rabbykhairul@gmail.com>
2020-09-26 14:34:15 +0600

    receive a img upload request & send a minimal feedback about the uploaded image

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+app.post('/api/resizeImage', upload.single('image'), (req, res) => {
+    const imgInfo = {
+        originalFileName: req.file.originalname,
+        ecoding: req.file.encoding,
+        mimetype: req.file.mimetype,
+        size: req.file.size,
+        path: req.file.path
+    };
+    res.send(imgInfo);
+});
 
 app.listen(port, () => {
     console.log(`Started listening at port ${port}...`);

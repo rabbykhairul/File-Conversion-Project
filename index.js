459b1a58020b674588260d3f40ce917fa0dcb091
rabbykhairul <rabbykhairul@gmail.com>
2020-09-26 18:19:08 +0600

    save uploaded image with required extension and allow access through url

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 const express = require('express');
 const multer = require('multer');
 const app = express();
-const upload = multer({dest: 'uploads/'});
 const port = process.env.PORT || 3000;
+const path = require('path');
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'public/uploads');
+    },
+    filename: function(req, file, cb) {
+        const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1E9);
+        cb(null, uniqueSuffix + path.extname(file.originalname));
+    }
+});
+
+const upload = multer({storage: storage});
 
 app.use(express.json());
+app.use(express.static('public'));
 
 app.post('/api/resizeImage', upload.single('image'), (req, res) => {
     const imgInfo = {
         originalFileName: req.file.originalname,
         ecoding: req.file.encoding,
         mimetype: req.file.mimetype,
-        size: req.file.size,
+        size: req.file.size + " bytes",
         path: req.file.path
     };
     res.send(imgInfo);

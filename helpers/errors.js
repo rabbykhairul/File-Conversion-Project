845f86d21c0721bf4c7fcf4db432dff219896c56
rabbykhairul <rabbykhairul@gmail.com>
2020-09-30 17:10:34 +0600

    pretify code

--- helpers/errors.js
+++ helpers/errors.js
@@ -1,22 +1,22 @@
 class ErrorHandler extends Error {
-    constructor(statusCode, message) {
-        super();
-        this.statusCode = statusCode;
-        this.message = message;
-    }
+  constructor(statusCode, message) {
+    super();
+    this.statusCode = statusCode;
+    this.message = message;
+  }
 }
 
 const handle_error = (err, res) => {
-    const { statusCode, message } = err;
-    const errorDetails = {
-        status: "Error",
-        statusCode,
-        message
-    };
-    res.status(statusCode).send(errorDetails);
+  const { statusCode, message } = err;
+  const errorDetails = {
+    status: "Error",
+    statusCode,
+    message,
+  };
+  res.status(statusCode).send(errorDetails);
 };
 
 module.exports = {
-    ErrorHandler,
-    handle_error
+  ErrorHandler,
+  handle_error,
 };
--- index.js
+++ index.js
@@ -1,63 +1,94 @@
-const express = require('express');
-const multer = require('multer');
-const { ErrorHandler, handle_error } = require('./helpers/errors');
+const express = require("express");
+const multer = require("multer");
+const { ErrorHandler, handle_error } = require("./helpers/errors");
 const app = express();
 const port = process.env.PORT || 3000;
-const path = require('path');
+const path = require("path");
 
 const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, 'public/uploads');
-    },
-    filename: function(req, file, cb) {
-        const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1E9);
-        cb(null, uniqueSuffix + path.extname(file.originalname));
-    }
+  destination: function (req, file, cb) {
+    cb(null, "public/uploads");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + path.extname(file.originalname));
+  },
 });
 
 const upload = multer({
-    storage: storage,
-    fileFilter: function(req, file, cb) {
-        is_valid_image(file, cb);
-    }
+  storage: storage,
+  fileFilter: function (req, file, cb) {
+    is_valid_image(file, cb);
+  },
 });
 
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 function is_valid_image(file, cb) {
-    const fileExt = path.extname(file.originalname).toLowerCase();
-    const fileMime = file.mimetype;
+  const fileExt = path.extname(file.originalname).toLowerCase();
+  const fileMime = file.mimetype;
 
-    const imageTypes = [ '.apng', '.bmp', '.gif', '.ico', '.cur', '.jpg', '.jpeg', '.jfif', '.pjpeg', '.pjp', '.png', '.tif', '.tiff', '.webp' ];
-    const mimetypes = [ 'image/apng', 'image/bmp', 'image/gif', 'image/x-icon', 'image/jpeg', 'image/png', 'image/tiff', 'image/webp' ];
+  const imageTypes = [
+    ".apng",
+    ".bmp",
+    ".gif",
+    ".ico",
+    ".cur",
+    ".jpg",
+    ".jpeg",
+    ".jfif",
+    ".pjpeg",
+    ".pjp",
+    ".png",
+    ".tif",
+    ".tiff",
+    ".webp",
+  ];
+  const mimetypes = [
+    "image/apng",
+    "image/bmp",
+    "image/gif",
+    "image/x-icon",
+    "image/jpeg",
+    "image/png",
+    "image/tiff",
+    "image/webp",
+  ];
 
-    if (imageTypes.includes(fileExt) && mimetypes.includes(fileMime)) {
-        return cb(null, true);
-    }
-    else {
-        return cb(new ErrorHandler(404, "The file selected in the image field is not an image. Please try again with an image file."));
-    }
+  if (imageTypes.includes(fileExt) && mimetypes.includes(fileMime)) {
+    return cb(null, true);
+  } else {
+    return cb(
+      new ErrorHandler(
+        404,
+        "The file selected in the image field is not an image. Please try again with an image file."
+      )
+    );
+  }
 }
 
-app.post('/api/resizeImage', upload.single('image'), (req, res) => {
-    if (req.file == undefined) {
-        throw new ErrorHandler(404, "You have not selected any image file. Please select an image from your device to sumbmit and try again.");
-    }
-    const imgInfo = {
-        originalFileName: req.file.originalname,
-        ecoding: req.file.encoding,
-        mimetype: req.file.mimetype,
-        size: req.file.size + " bytes",
-        path: req.file.path
-    };
-    res.send(imgInfo);
+app.post("/api/resizeImage", upload.single("image"), (req, res) => {
+  if (req.file == undefined) {
+    throw new ErrorHandler(
+      404,
+      "You have not selected any image file. Please select an image from your device to sumbmit and try again."
+    );
+  }
+  const imgInfo = {
+    originalFileName: req.file.originalname,
+    ecoding: req.file.encoding,
+    mimetype: req.file.mimetype,
+    size: req.file.size + " bytes",
+    path: req.file.path,
+  };
+  res.send(imgInfo);
 });
 
 app.use((err, req, res, next) => {
-    handle_error(err, res);
-})
+  handle_error(err, res);
+});
 
 app.listen(port, () => {
-    console.log(`Started listening at port ${port}...`);
-})
+  console.log(`Started listening at port ${port}...`);
+});
